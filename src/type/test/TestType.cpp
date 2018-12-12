f7d4a123479f0a284f5d342094a24e2bbf5fc6f9
Shawn Cao <caoxhua@gmail.com>
2018-12-12 03:19:04 -0800

    Test on std::tuple
    - Do a bit futher research on tuple

--- src/type/test/TestType.cpp
+++ src/type/test/TestType.cpp
@@ -16,6 +16,10 @@
 
 #include "gtest/gtest.h"
 #include <any>
+#include <cstddef>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 #include <vector>
 #include "Errors.h"
 #include "Tree.h"
@@ -71,6 +75,84 @@ TEST(VectorTest, CxxVersion) {
   vec.push_back(1);
   vec.push_back("something");
   EXPECT_EQ(vec.size(), 2);
+}
+
+/*
+* To build a heterogenous container, we have choices
+* - static fixed size: 
+* -- std::tuple
+* -- variadic template parameter pack
+* - dynamic sized: 
+* -- std::any of an existing contianer such as vector (any_cast)
+* -- std::variant with known type set std::variant<T1, T2...>
+* this test is on tuple - the pattern comes from cpppatterns.com
+*/
+template <typename F, typename Tuple, size_t... S>
+decltype(auto) apply_tuple_impl(F&& fn, Tuple&& t, std::index_sequence<S...>) {
+  return std::forward<F>(fn)(std::get<S>(std::forward<Tuple>(t))...);
+}
+
+template <typename F, typename Tuple>
+decltype(auto) apply_from_tuple(F&& fn, Tuple&& t) {
+  std::size_t constexpr tSize = std::tuple_size<typename std::remove_reference<Tuple>::type>::value;
+  return apply_tuple_impl(std::forward<F>(fn),
+                          std::forward<Tuple>(t),
+                          std::make_index_sequence<tSize>());
+}
+
+template <typename T>
+void handle(T v);
+
+template <>
+void handle(int v) {
+  LOG(INFO) << "INT=" << v;
+}
+
+template <>
+void handle(bool v) {
+  LOG(INFO) << "BOOL=" << v;
+}
+
+template <>
+void handle(char const* v) {
+  LOG(INFO) << "STR=" << v;
+}
+
+template <>
+void handle(double v) {
+  LOG(INFO) << "FLOAT=" << v;
+}
+
+#define DI(I)                \
+  if constexpr (size > I) {  \
+    handle(std::get<I>(tp)); \
+  }
+
+template <typename Tuple>
+decltype(auto) loop(Tuple&& tp) {
+  std::size_t constexpr size = std::tuple_size<typename std::remove_reference_t<Tuple>>::value;
+  DI(0)
+  DI(1)
+  DI(2)
+  DI(3)
+  DI(4)
+  DI(5)
+}
+
+#undef DI
+
+TEST(TupleTest, TestTuple) {
+  auto sum = [](int a, int b, int c, int d) {
+    LOG(INFO) << "a=" << a << ", b=" << b;
+    return a + b + c + d;
+  };
+
+  int result = apply_from_tuple(sum, std::make_tuple(10, 20, 30, 40));
+  EXPECT_EQ(result, 100);
+
+  // test loop on each item
+  auto t = std::make_tuple(10, true, "C++", 1.0);
+  loop(t);
 }
 
 } // namespace test
