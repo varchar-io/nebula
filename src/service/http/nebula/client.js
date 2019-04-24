6380a5993103111689728cf14ad01b7cbb098444
Shawn Cao <caoxhua@gmail.com>
2019-04-24 13:25:57 -0700

    Build web version nebula client as NebulaClient

--- src/service/http/nebula/client.js
+++ src/service/http/nebula/client.js
@@ -10,58 +10,12 @@ const {
     V1Client
 } = require('../../gen/nebula/nebula_grpc_web_pb.js');
 
-var serviceAddr = "http://dev-shawncao:8080";
-var client = new EchoClient(serviceAddr);
-
-// simple unary call
-var request = new EchoRequest();
-request.setName('Trends On Nebula');
-
-client.echoBack(request, {}, (err, response) => {
-    var display = document.getElementById("output");
-    if (err !== null) {
-        display.innerText = "Error code: " + err;
-    } else {
-        display.innerText = (response == null) ? "Failed to get reply" : response.getMessage();
+export default {
+    EchoClient,
+    V1Client,
+    EchoRequest,
+    TableStateRequest,
+    test: function (a, b) {
+        return a * b;
     }
-});
-
-var v1Client = new V1Client(serviceAddr);
-var req = new TableStateRequest();
-req.setTable("pin.trends");
-
-// call the service
-v1Client.state(req, {}, (err, reply) => {
-    var result = document.getElementById("result");
-    if (err !== null) {
-        result.innerText = "Error code: " + err;
-    } else if (reply == null) {
-        result.innerText = "Failed to get reply";
-    } else {
-        result.innerText = "Blocks: " + reply.getBlockcount() + ", Rows: " + reply.getRowcount() + ", Size: " + reply.getMemsize() + " bytes";
-    }
-});
-
-
-// server streaming call
-// var streamRequest = new RepeatHelloRequest();
-// streamRequest.setName('World');
-// streamRequest.setCount(5);
-
-// var stream = client.sayRepeatHello(streamRequest, {});
-// stream.on('data', (response) => {
-//     console.log(response.getMessage());
-// });
-
-
-// // deadline exceeded
-// var deadline = new Date();
-// deadline.setSeconds(deadline.getSeconds() + 1);
-
-// client.sayHelloAfterDelay(request, {
-//         deadline: deadline.getTime()
-//     },
-//     (err, response) => {
-//         console.log('Got error, code = ' + err.code +
-//             ', message = ' + err.message);
-//     });
+};
--- src/service/http/nebula/webpack.config.js
+++ src/service/http/nebula/webpack.config.js
@@ -2,10 +2,21 @@ var path = require('path');
 
 module.exports = {
     mode: "development",
+    entry: {
+        client: './client.js',
+    },
+    output: {
+        filename: './main.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'NebulaClient',
+        libraryExport: 'default'
+    },
     resolve: {
         alias: {
             "google-protobuf": path.resolve(__dirname, './node_modules/google-protobuf/'),
             "grpc-web": path.resolve(__dirname, './node_modules/grpc-web/'),
+            "nebula-pb": path.resolve(__dirname, '../../gen/nebula/nebula_pb.js'),
+            "nebula-rpc": path.resolve(__dirname, '../../gen/nebula/nebula_grpc_web_pb.js'),
         },
         extensions: [
             ".ts",
