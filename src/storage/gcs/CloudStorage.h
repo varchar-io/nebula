66e3eac493c5de30959c82ae821d87ee24d7846a
Feng Yu <feny@google.com>
2020-12-06 07:17:41 +0000

    Add GCS support for nebula.

--- src/storage/gcs/CloudStorage.h
+++ src/storage/gcs/CloudStorage.h
@@ -18,6 +18,10 @@
 
 #include <mutex>
 
+<<<<<<< HEAD
+=======
+#include "common/Errors.h"
+>>>>>>> 763056e (Add GCS support for nebula.)
 #include "google/cloud/storage/client.h"
 #include "storage/NFileSystem.h"
 
@@ -29,9 +33,14 @@ namespace storage {
 namespace gcs {
 class CloudStorage : public NFileSystem {
 public:
+<<<<<<< HEAD
   CloudStorage(google::cloud::storage::Client* gcs_client,
                const std::string& bucket) : gcs_client_(gcs_client),
     bucket_(bucket) {}
+=======
+  CloudStorage(google::cloud::storage::Client* gcs_client, 
+               const std::string& bucket) : bucket_{ bucket } {}
+>>>>>>> 763056e (Add GCS support for nebula.)
   virtual ~CloudStorage() = default;
 
 public:
@@ -39,15 +48,29 @@ public:
   // if obj is true, it will return all objects (max 1K) under given prefix at any level
   // otherwise it will only return sub-prefixes one level down under current prefix
   virtual std::vector<FileInfo> list(const std::string&) override;
+<<<<<<< HEAD
 
   void read(const std::string&, const std::string&);
   // read a file/object at given offset and length into buffer address provided
   virtual size_t read(const std::string&, const size_t, const size_t, char*) override;
+=======
+  void read(const std::string&, const std::string&);
+  // read a file/object at given offset and length into buffer address provided
+  virtual size_t read(const std::string&, const size_t, const size_t, char*) override {
+    throw NException("Not implemented");
+  }
+>>>>>>> 763056e (Add GCS support for nebula.)
 
   // read a file/object fully into a memory buffer
   virtual size_t read(const std::string&, char*, size_t) override;
 
+<<<<<<< HEAD
   virtual FileInfo info(const std::string&) override;
+=======
+  virtual FileInfo info(const std::string&) override {
+    throw NException("Not implemented");
+  }
+>>>>>>> 763056e (Add GCS support for nebula.)
 
   // download a prefix to a local tmp file
   virtual bool copy(const std::string&, const std::string&) override;
@@ -61,17 +84,30 @@ public:
   virtual bool sync(const std::string&, const std::string&, bool recursive = false) override;
 
   // remove a s3 file or s3 prefix
+<<<<<<< HEAD
   virtual void rm(const std::string&) override;
 
 private:
   void downloadFile(const std::string&, const std::string&);
   void uploadFile(const std::string&, const std::string&);
+=======
+  virtual void rm(const std::string&) override {
+    throw NException("Not implemented");
+  }
+
+private:
+  void download(const std::string&, const std::string&);
+  void upload(const std::string&, const std::string&);
+>>>>>>> 763056e (Add GCS support for nebula.)
 
 private:
   google::cloud::storage::Client* gcs_client_;
   std::string bucket_;
+<<<<<<< HEAD
 
   std::mutex local_file_mutex_;
+=======
+>>>>>>> 763056e (Add GCS support for nebula.)
 };
 } // namespace gcs
 } // namespace storage
