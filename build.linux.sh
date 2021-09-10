7dd95ab811ce6fe541acf6aa4c9c0ad0526f68c1
Shawn Cao <caoxhua@gmail.com>
2021-09-10 13:03:47 -0700

    move abseil and crc32 installation to build script

--- build.linux.sh
+++ build.linux.sh
@@ -90,6 +90,28 @@ done
   fi
 )
 
+# Install Abseil
+(
+  if [ -z "$(ls -A ./abseil-cpp)" ]; then
+    git clone --depth 1 --branch 20200923.3 https://github.com/abseil/abseil-cpp.git
+    cd abseil-cpp && mkdir build && cd build
+    cmake -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DABSL_USES_STD_STRING_VIEW=ON -DABSL_USES_STD_OPTIONAL=ON -DCMAKE_CXX_STANDARD=11 ..
+    make -j$(nproc)
+    sudo make install
+  fi
+)
+
+# Install crc32c
+(
+  if [ -z "$(ls -A ./crc32c)" ]; then
+    git clone --depth 1 --branch 1.1.1 https://github.com/google/crc32c.git
+    cd crc32c && mkdir build && cd build
+    cmake -DCRC32C_BUILD_TESTS=OFF -DCRC32C_BUILD_BENCHMARKS=OFF ..
+    make -j$(nproc)
+    sudo make install
+  fi
+)
+
 # run nebula cmake
 echo "enter password for sudo..."
 echo "-----------------------"
--- ext/Gcp_Ext.cmake
+++ ext/Gcp_Ext.cmake
@@ -2,40 +2,6 @@ find_package(Threads REQUIRED)
 
 include(ExternalProject)
 
-# install abseil (required >= c++ 11)
-# Note: use 11 rather than 17 so that we can use absl::string_view rather than std::string_view
-# because GCP common references absl::string_view directly
-SET(ABSL_OPTS
-    -DBUILD_TESTING=OFF
-    -DBUILD_SHARED_LIBS=OFF
-    -DABSL_USES_STD_STRING_VIEW=ON
-    -DABSL_USES_STD_OPTIONAL=ON
-    -DCMAKE_CXX_STANDARD=11)
-ExternalProject_Add(absl
-    PREFIX absl
-    GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git
-    GIT_TAG 20200923.3
-    CMAKE_ARGS ${ABSL_OPTS}
-    INSTALL_DIR ${NEBULA_INSTALL}
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON)
-
-# install crc32
-SET(CRC32C_OPTS
-    -DCRC32C_BUILD_TESTS=OFF
-    -DCRC32C_BUILD_BENCHMARKS=OFF)
-ExternalProject_Add(crc32c
-    PREFIX crc32c
-    GIT_REPOSITORY https://github.com/google/crc32c.git
-    GIT_TAG 1.1.1
-    CMAKE_ARGS ${CRC32C_OPTS}
-    UPDATE_COMMAND ""
-    INSTALL_DIR ${NEBULA_INSTALL}
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON)
-
 # include nlohmann json
 ExternalProject_Add(nlohmann
     PREFIX nlohmann
