0aa2aada393e5e503a1b6f270ccbc1feb8428b9a
Shawn Cao <caoxhua@gmail.com>
2021-11-04 16:54:55 -0700

    move the k8s example to deploy folder

---
+++ deploy/install-latest-docker-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# Install latest docker-compose that we may need to support new syntax in docker-compose build
+
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
+alias docker-compose=$DESTINATION
